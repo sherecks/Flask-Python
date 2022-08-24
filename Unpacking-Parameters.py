d4ccd660aac5a30087968120c6484d4840301483
João Pedro Patricio do Nascimento <joaoppn00@gmail.com>
2022-08-24 20:42:54 -0300

    Ponteiros 2.0

--- Unpacking-Parameters.py
+++ Unpacking-Parameters.py
@@ -1,6 +1,21 @@
-def add(x, y):
-  return x + y
+from inspect import ArgSpec
 
 
-nums = {"x": 15, "y": 25}
-print(add(**nums))
+def multi(*args):
+  for arg in args:
+    total = total * arg
+
+  return total
+
+
+def aplicar(*args, operador):
+  if operador == "*":
+    return multi(args)
+  elif operador == "+":
+    return sum(args)
+  else:
+    return "Operador não está valido para aplicação"
+
+
+
+print(aplicar(2, 4, 6, 8, operador="+"))    
