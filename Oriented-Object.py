77574af235c8fa71eddcad26dff217f8cf586530
João Pedro Patricio do Nascimento <joaoppn00@gmail.com>
2022-09-14 18:44:15 -0300

    Oriented Objects 2.0

--- Oriented-Object.py
+++ Oriented-Object.py
@@ -1,13 +1,20 @@
 class Student:
-  def __init__(self):
-    self.name = "Pietra"
-    self.notas = (5, 10, 4, 7, 6, 5)
+  def __init__(self, name, notas):
+    self.name = name
+    self.notas = notas
 
   def average(self):
     return sum(self.notas) / len(self.notas)
 
 
-student = Student()
+student = Student("Pietra", (10, 8, 7, 5, 9, 7))
+
+student2 = Student("João Pedro", (7, 6, 5, 6, 8, 7))
+
 print(f"Nome: {student.name}")
 print(f"Notas: {student.notas}")
 print(f"Media: {student.average()}")
+
+print(f"Nome: {student2.name}")
+print(f"Notas: {student2.notas}")
+print(f"Media: {student2.average()}")
