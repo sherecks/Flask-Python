f110f0771d5b3a9a263a13eb523c74b99ff84373
João Pedro Patrício do Nascimento <joaoppn00@gmail.com>
2022-09-21 21:10:57 -0300

    Teste Make_Secure Decorators

--- Decorators.py
+++ Decorators.py
@@ -1,14 +1,18 @@
-user = {"username": "João", "acess_level": "admin"}
+user = {"username": "João", "acess_level": "guest"}
 
 def get_admin_password():
     return "Rubicho25!"
 
+def make_secure(func):
+    def secure_function():
+        if user["acess_level"] == "admin":
+            return func()
+        else:
+            return f"No admin permissions for {user['username']}"
 
-def secure_function(func):
-    if user["acess_level"] == "admin":
-        return func
+    return secure_function    
 
 
-get_admin_password = secure_function(get_admin_password)
+get_admin_password = make_secure(get_admin_password)
 
 print(get_admin_password())
